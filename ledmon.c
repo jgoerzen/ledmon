5b0cf40920f992038ce04551789508ab7655e477
John Goerzen <jgoerzen@complete.org>
2008-09-16 00:09:05 -0500

    Working

--- ledmon.c
+++ ledmon.c
@@ -2,12 +2,16 @@
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
 #include <X11/XKBlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 enum {
   CapsLock = 0,
   NumLock,
   ScrollLock
 };
+
+void displayState(unsigned int state);
 
 static int xkb_event_base = 0;
 static int xkb_error_base = 0;
@@ -37,21 +41,37 @@ void main(void) {
   }
 
   if (!XkbSelectEvents(disp, XkbUseCoreKbd, XkbIndicatorStateNotifyMask, 
-                       XkbIndicatorStateNotifyMast)) {
+                       XkbIndicatorStateNotifyMask)) {
     fprintf(stderr, "XkbSelectEvents\n");
     exit(1);
   }
 
-  XlbGetIndicatorState(disp, XkbUseCoreKdb, &state);
+  XkbGetIndicatorState(disp, XkbUseCoreKbd, &state);
   displayState(state);
   
-  win = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, 200, 300, 5, white,
+  /*
+  win = XCreateSimpleWindow(disp, DefaultRootWindow(display), 0, 0, 200, 300, 5, white,
                             black);
-  
+  */
+
   while (1) {
-    XNextEvent(display, &ev.core);
+    XNextEvent(disp, &ev.core);
     if (ev.type == xkb_event_base && ev.any.xkb_type == XkbIndicatorStateNotify) {
       displayState(ev.indicators.state);
     }
   }
+}
+
+void displayState(unsigned int state) {
+  if (state & (1 << 0)) {
+    printf("CAPS ");
+  }
+  if (state & (1 << 1)) {
+    printf("NUM ");
+  }
+  if (state & (1 << 2)) {
+    printf("SCROLL");
+  }
+  printf("\n");
+
 }
